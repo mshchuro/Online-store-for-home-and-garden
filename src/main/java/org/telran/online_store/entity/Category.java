e876288e90e61139d6a7ad6a9513aa78375012c4
VladaIs <vl.f.95@inbox.lv>
2025-04-04 18:25:44 +0200

    category added

---
+++ src/main/java/org/telran/online_store/entity/Category.java
@@ -0,0 +1,32 @@
+package org.telran.online_store.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+
+    public class Category {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+        private Long id;
+
+        private String name;
+
+        private String description;
+
+        @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+        private List<Product> products;
+    }
