df6d861b38aba39e700c2646c8dab39842b3399a
VladaIs <vl.f.95@inbox.lv>
2025-04-07 22:28:49 +0200

    controller, repository, service added

--- src/main/java/org/telran/online_store/controller/CategoryController.java
+++ src/main/java/org/telran/online_store/controller/CategoryController.java
@@ -1,5 +1,8 @@
 package org.telran.online_store.controller;
 
+import jakarta.servlet.ServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.telran.online_store.entity.Category;
 import org.telran.online_store.service.CategoryService;
@@ -7,41 +10,34 @@ import org.telran.online_store.service.CategoryService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categories")
+@RequestMapping("/api/categories")
 public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @Autowired
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
     @GetMapping
-    public List<Category> getAll() {
-
-        return null;
+    public List<Category> getAll(ServletRequest servletRequest) {
+        return categoryService.getAllCategories();
     }
 
     @GetMapping("/{id}")
-    public Category getById(@PathVariable Long id) {
-
-        return null;
+    public Category getCategoryById(@PathVariable Long id) {
+        return categoryService.getCategoryById(id);
     }
 
     @PostMapping
-    public Category create(@RequestBody Category category) {
-
-        return null;
-    }
-
-    @PutMapping("/{id}")
-    public Category update(@PathVariable Long id, @RequestBody Category category) {
-
-        return null;
+    public ResponseEntity<Category> create(@RequestBody Category category) {
+        return ResponseEntity.ok(categoryService.createCategory(category));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.accepted().build();
     }
 }
