91da23639dd5daffeac8b9ddce2b5a78839600b5
Julia Frolova <jmabracodabra@gmail.com>
2025-04-21 20:56:12 +0200

    test

--- src/main/java/org/telran/online_store/exception/CategoryNotFoundException.java
+++ src/main/java/org/telran/online_store/exception/CategoryNotFoundException.java
@@ -1,7 +1,6 @@
 package org.telran.online_store.exception;
 
 public class CategoryNotFoundException extends RuntimeException {
-
     public CategoryNotFoundException(String message) {
         super(message);
     }
