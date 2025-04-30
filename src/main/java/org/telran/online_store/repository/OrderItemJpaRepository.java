473c9e96037061ba83bc65c44b2b5217421594e0
mshchuro <150063341+mshchuro@users.noreply.github.com>
2025-05-01 00:06:47 +0300

    Rename getAll to getUserHistory, method returns only current user orders (#28)
    Add getAll only for Administrator to check all orders
    Now getStatus return only order status
    
    Co-authored-by: mshchurovskiy <mshchuro@icloud.com>
    Co-authored-by: Julia Frolova <jmabracodabra@gmail.com>

--- src/main/java/org/telran/online_store/repository/OrderItemJpaRepository.java
+++ src/main/java/org/telran/online_store/repository/OrderItemJpaRepository.java
@@ -1,7 +1,9 @@
 package org.telran.online_store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.telran.online_store.entity.OrderItem;
 
+@Repository
 public interface OrderItemJpaRepository extends JpaRepository<OrderItem, Long> {
 }
--- src/main/java/org/telran/online_store/service/OrderService.java
+++ src/main/java/org/telran/online_store/service/OrderService.java
@@ -1,13 +1,11 @@
 package org.telran.online_store.service;
 
-import org.aspectj.weaver.ast.Or;
-import org.telran.online_store.dto.OrderRequestDto;
 import org.telran.online_store.entity.Order;
-import org.telran.online_store.entity.Product;
-
 import java.util.List;
 
 public interface OrderService {
+
+    List<Order> getAll();
 
     List<Order> getAllUserOrders();
 
