b6366e782627fb3b56d90fc21d64f9cc086cdb9f
VladaIs <vl.f.95@inbox.lv>
2025-05-12 23:12:01 +0200

    add deleteCart() in CartController

--- src/main/java/org/telran/online_store/controller/CartController.java
+++ src/main/java/org/telran/online_store/controller/CartController.java
@@ -37,8 +37,8 @@ public class CartController {
     }
 
     @DeleteMapping()
-    public ResponseEntity<Void> deleteCart() {
-        cartService.clearCart();
-        return ResponseEntity.status(HttpStatus.OK).build();
+    public ResponseEntity<CartResponseDto> deleteCart() {
+        Cart cart = cartService.clearCart();
+        return ResponseEntity.status(HttpStatus.OK).body(cartConverter.toDto(cart));
     }
 }
