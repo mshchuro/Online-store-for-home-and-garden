9b622c907e214f776a631a3f0ad82ebec3a6d569
Julia Frolova <jmabracodabra@gmail.com>
2025-05-21 12:02:51 +0200

    SchedulerConfig moved to config package

--- src/main/java/org/telran/online_store/config/SchedulerConfig.java
+++ src/main/java/org/telran/online_store/config/SchedulerConfig.java
@@ -1,4 +1,4 @@
-package org.telran.online_store.scheduler;
+package org.telran.online_store.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
